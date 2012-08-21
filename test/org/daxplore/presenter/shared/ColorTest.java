1572424702da0b564566823ab7e7e615a9f713e8
Daniel Dunér <daniel.duner@gmail.com>
2012-08-21 02:02:28 +0200

    Clean up ColorCode test cases, to test the actual code rather than the help methods

--- test/org/daxplore/presenter/shared/ColorTest.java
+++ test/org/daxplore/presenter/shared/ColorTest.java
@@ -20,6 +20,7 @@ package org.daxplore.presenter.shared;
 
 import static org.junit.Assert.*;
 
+import org.daxplore.presenter.shared.Color.Model;
 import org.junit.Test;
 
 public class ColorTest {
@@ -29,26 +30,26 @@ public class ColorTest {
 	
 	// Example values taken from http://en.wikipedia.org/wiki/HSL_color_space#Examples
 	private static final double[][] values = {
-	//	{r		g		b		h		v		l		hsv_s	hsl_s}
-		{1.000,	1.000,	1.000,	0,		1.000,	1.000,	0.000,	0.000},
-		{0.500,	0.500,	0.500,	0,		0.500,	0.500,	0.000,	0.000},
-		{0.000,	0.000,	0.000,	0,		0.000,	0.000,	0.000,	0.000},
-		{1.000,	0.000,	0.000,	0.0,	1.000,	0.500,	1.000,	1.000},
-		{0.750,	0.750,	0.000,	60.0,	0.750,	0.375,	1.000,	1.000},
-		{0.000,	0.500,	0.000,	120.0,	0.500,	0.250,	1.000,	1.000},
-		{0.500,	1.000,	1.000,	180.0,	1.000,	0.750,	0.500,	1.000},
-		{0.500,	0.500,	1.000,	240.0,	1.000,	0.750,	0.500,	1.000},
-		{0.750,	0.250,	0.750,	300.0,	0.750,	0.500,	0.667,	0.500},
-		{0.628,	0.643,	0.142,	61.8,	0.643,	0.393,	0.779,	0.638},
-		{0.255,	0.104,	0.918,	251.1,	0.918,	0.511,	0.887,	0.832},
-		{0.116,	0.675,	0.255,	134.9,	0.675,	0.396,	0.828,	0.707},
-		{0.941,	0.785,	0.053,	49.5,	0.941,	0.497,	0.944,	0.893},
-		{0.704,	0.187,	0.897,	283.7,	0.897,	0.542,	0.792,	0.775},
-		{0.931,	0.463,	0.316,	14.3,	0.931,	0.624,	0.661,	0.817},
-		{0.998,	0.974,	0.532,	56.9,	0.998,	0.765,	0.467,	0.991},
-		{0.099,	0.795,	0.591,	162.4,	0.795,	0.447,	0.875,	0.779},
-		{0.211,	0.149,	0.597,	248.3,	0.597,	0.373,	0.750,	0.601},
-		{0.495,	0.493,	0.721,	240.5,	0.721,	0.607,	0.316,	0.290}};
+	//	{r				g				b				h		v		l		hsv_s	hsl_s}
+		{255 * 1.000,	255 * 1.000,	255 * 1.000,	0,		1.000,	1.000,	0.000,	0.000},
+		{255 * 0.500,	255 * 0.500,	255 * 0.500,	0,		0.500,	0.500,	0.000,	0.000},
+		{255 * 0.000,	255 * 0.000,	255 * 0.000,	0,		0.000,	0.000,	0.000,	0.000},
+		{255 * 1.000,	255 * 0.000,	255 * 0.000,	0.0,	1.000,	0.500,	1.000,	1.000},
+		{255 * 0.750,	255 * 0.750,	255 * 0.000,	60.0,	0.750,	0.375,	1.000,	1.000},
+		{255 * 0.000,	255 * 0.500,	255 * 0.000,	120.0,	0.500,	0.250,	1.000,	1.000},
+		{255 * 0.500,	255 * 1.000,	255 * 1.000,	180.0,	1.000,	0.750,	0.500,	1.000},
+		{255 * 0.500,	255 * 0.500,	255 * 1.000,	240.0,	1.000,	0.750,	0.500,	1.000},
+		{255 * 0.750,	255 * 0.250,	255 * 0.750,	300.0,	0.750,	0.500,	0.667,	0.500},
+		{255 * 0.628,	255 * 0.643,	255 * 0.142,	61.8,	0.643,	0.393,	0.779,	0.638},
+		{255 * 0.255,	255 * 0.104,	255 * 0.918,	251.1,	0.918,	0.511,	0.887,	0.832},
+		{255 * 0.116,	255 * 0.675,	255 * 0.255,	134.9,	0.675,	0.396,	0.828,	0.707},
+		{255 * 0.941,	255 * 0.785,	255 * 0.053,	49.5,	0.941,	0.497,	0.944,	0.893},
+		{255 * 0.704,	255 * 0.187,	255 * 0.897,	283.7,	0.897,	0.542,	0.792,	0.775},
+		{255 * 0.931,	255 * 0.463,	255 * 0.316,	14.3,	0.931,	0.624,	0.661,	0.817},
+		{255 * 0.998,	255 * 0.974,	255 * 0.532,	56.9,	0.998,	0.765,	0.467,	0.991},
+		{255 * 0.099,	255 * 0.795,	255 * 0.591,	162.4,	0.795,	0.447,	0.875,	0.779},
+		{255 * 0.211,	255 * 0.149,	255 * 0.597,	248.3,	0.597,	0.373,	0.750,	0.601},
+		{255 * 0.495,	255 * 0.493,	255 * 0.721,	240.5,	0.721,	0.607,	0.316,	0.290}};
 
 	@Test
 	public void testRGBtoHex() {
@@ -66,29 +67,11 @@ public class ColorTest {
 	public void testRgbToHsl() {
 		for (int line = 0; line<values.length; line++) {
 			double[] v = values[line];
-			int r = (int)(255*v[R] + 0.5);
-			int g = (int)(255*v[G] + 0.5);
-			int b = (int)(255*v[B] + 0.5);
-			double[] hsl = Color.rgbToHsl(r, g, b);
-
-			assertEquals(v[H],		hsl[0], 0.5);
-			assertEquals(v[HSL_S], 	hsl[1], 0.01);
-			assertEquals(v[L], 		hsl[2], 0.01);
-		}
-	}
-
-	@Test
-	public void testRgbToHsv() {
-		for (int line = 0; line<values.length; line++) {
-			double[] v = values[line];
-			int r = (int)(255*v[R] + 0.5);
-			int g = (int)(255*v[G] + 0.5);
-			int b = (int)(255*v[B] + 0.5);
-			double[] hsv = Color.rgbToHsv(r, g, b);
+			Color color = new Color(v[R], v[G], v[B]);
 			
-			assertEquals(v[H], 		hsv[0], 0.5);
-			assertEquals(v[HSV_S],	hsv[1], 0.01);
-			assertEquals(v[V],		hsv[2], 0.01);
+			assertEquals(v[H],		color.getHSLHue(),			0.5);
+			assertEquals(v[HSL_S], 	color.getHSLSaturation(),	0.005);
+			assertEquals(v[L], 		color.getHSLLightness(),	0.005);
 		}
 	}
 	
@@ -97,11 +80,23 @@ public class ColorTest {
 	public void testHslToRgb() {
 		for (int line = 0; line<values.length; line++) {
 			double[] v = values[line];
-			double[] rgb = Color.hslToRgb(v[H], v[HSL_S], v[L]);
-
-			assertEquals(v[R]*255, rgb[0], 0.5);
-			assertEquals(v[G]*255, rgb[1], 0.5);
-			assertEquals(v[B]*255, rgb[2], 0.5);
+			Color color = new Color(v[H], v[HSL_S], v[L], Model.HSL);
+			
+			assertEquals(v[R],	color.getRed(),		0.5);
+			assertEquals(v[G], 	color.getGreen(),	0.5);
+			assertEquals(v[B], 	color.getBlue(),	0.5);
+		}
+	}
+	
+	@Test
+	public void testRgbToHsv() {
+		for (int line = 0; line<values.length; line++) {
+			double[] v = values[line];
+			Color color = new Color(v[R], v[G], v[B]);
+			
+			assertEquals(v[H],		color.getHSVHue(),			0.5);
+			assertEquals(v[HSV_S], 	color.getHSVSaturation(),	0.005);
+			assertEquals(v[V], 		color.getHSVValue(),		0.005);
 		}
 	}
 	
@@ -109,11 +104,12 @@ public class ColorTest {
 	public void testHsvToRgb() {
 		for (int line = 0; line<values.length; line++) {
 			double[] v = values[line];
-			double[] rgb = Color.hsvToRgb(v[H], v[HSV_S], v[V]);
+			Color color = new Color(v[H], v[HSV_S], v[V], Model.HSV);
 			
-			assertEquals(v[R]*255, rgb[0], 0.5);
-			assertEquals(v[G]*255, rgb[1], 0.5);
-			assertEquals(v[B]*255, rgb[2], 0.5);
+			assertEquals(v[R],	color.getRed(),		0.5);
+			assertEquals(v[G], 	color.getGreen(),	0.5);
+			assertEquals(v[B], 	color.getBlue(),	0.5);
 		}
 	}
+	
 }
