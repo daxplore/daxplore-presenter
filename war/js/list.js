9eae89033ad28587384940d168f7efa98937360f
Daniel Dunér <daniel.duner@gmail.com>
2016-10-12 15:14:52 +0200

    Add image saving

--- war/js/list.js
+++ war/js/list.js
@@ -15,7 +15,7 @@
     colors.bad_text     = "hsl( 5, 38%, 42%)";
   
     var data, perspective_option;
-    var chartBB, xAxisTopHeight, xAxisBottomHeight, margin, width, height;
+    var charwrapperBB, xAxisTopHeight, xAxisBottomHeight, margin, width, height;
     var x_scale, y_scale;
     var yAxisWidth;
     
@@ -26,7 +26,7 @@
 
     function setToReferenceColor(i) {
       d3.select("barrect-" + i)
-        .style("fill", colorForValue(data.reference_map[i], data.reference_map[i], data.direction_map[i]));
+        .style("fill", colorForValue(data.reference_map[i], data.reference_map[i], data.direction_map[data.q_ids[i]]));
     }
     
     function setToNormalColor(i) {
@@ -48,7 +48,7 @@
     }
         
     function colorForValue(value, reference, direction) {
-      if (direction == "low") {
+      if (direction == "LOW") {
         var diff = reference - value;
       } else {
         var diff = value - reference;
@@ -64,7 +64,7 @@
     }
     
     function colorHoverForValue(value, reference, direction) {
-      if (direction == "low") {
+      if (direction == "LOW") {
         var diff = reference - value;
       } else {
         var diff = value - reference;
@@ -80,7 +80,7 @@
     }
     
     function colorTextForValue(value, reference, direction) {
-      if (direction == "low") {
+      if (direction == "LOW") {
         var diff = reference - value;
       } else {
         var diff = value - reference;
@@ -105,8 +105,8 @@
       
       tooltipdiv.html(data.shorttexts[i] + ": <b>" + d3.format(".2s")(data.means[i][perspective_option]) + "</b><br>Referensvärde: <b>" + d3.format(".2")(data.references[i]) + "</b>")
         .style("background", colorHoverForValue(data.means[i][perspective_option], data.references[i], data.direction_map[data.q_ids[i]]))
-        .style("left", (chartBB.left + x_scale(Math.max(data.means[i][perspective_option], data.references[i])) + yAxisWidth + 14) + "px")   
-        .style("top", chartBB.top +  y_scale(data.q_ids[i]) + y_scale.bandwidth()/2 - tooltipdiv.node().getBoundingClientRect().height/2 + "px");
+        .style("left", (charwrapperBB.left + x_scale(Math.max(data.means[i][perspective_option], data.references[i])) + yAxisWidth + 14) + "px")   
+        .style("top", charwrapperBB.top +  y_scale(data.q_ids[i]) + y_scale.bandwidth()/2 - tooltipdiv.node().getBoundingClientRect().height/2 + "px");
       
       var arrowleft = d3.select(".arrow-left");
       
@@ -116,14 +116,14 @@
       
       arrowleft
         .style("border-right-color", colorHoverForValue(data.means[i][perspective_option], data.references[i], data.direction_map[data.q_ids[i]]))
-        .style("left", (chartBB.left + x_scale(Math.max(data.means[i][perspective_option], data.references[i])) + yAxisWidth + 4) + "px")   
-        .style("top", chartBB.top +  y_scale(data.q_ids[i]) + y_scale.bandwidth()/2 - arrowleft.node().getBoundingClientRect().height/2 + "px");
+        .style("left", (charwrapperBB.left + x_scale(Math.max(data.means[i][perspective_option], data.references[i])) + yAxisWidth + 4) + "px")   
+        .style("top", charwrapperBB.top +  y_scale(data.q_ids[i]) + y_scale.bandwidth()/2 - arrowleft.node().getBoundingClientRect().height/2 + "px");
         
       var description = d3.select(".description");
       
       description.transition()
-          .duration(0)
-          .style("opacity", 1);
+        .duration(0)
+        .style("opacity", 1);
       
       var color = colorTextForValue(data.means[i][perspective_option], data.references[i], data.direction_map[data.q_ids[i]]);
       var header = "<span class='description-header'>" + data.perspective_options[perspective_option] + "</span><br><b>" + data.shorttext_map[data.q_ids[i]] + ": " + d3.format(".2")(data.means[i][perspective_option]) + "</b><br>"
@@ -131,7 +131,7 @@
       
       
       var trueDiff = data.means[i][perspective_option] - data.references[i]; 
-      if (data.direction_map[data.q_ids[i]] == "low") {
+      if (data.direction_map[data.q_ids[i]] == "LOW") {
         var diff = data.references[i] - data.means[i][perspective_option]; 
       } else {
         var diff = trueDiff; 
@@ -171,14 +171,14 @@
     
     // DEFINITIONS
       
-    chartBB = d3.select(".chart-wrapper").node().getBoundingClientRect();
+    charwrapperBB = d3.select(".chart-wrapper").node().getBoundingClientRect();
     
     
     xAxisTopHeight = 30,
     xAxisBottomHeight = 24,
-    margin = {top: 0, right: 11, bottom: xAxisTopHeight + xAxisBottomHeight, left: 0},
-    width = chartBB.width - margin.left - margin.right,
-    height = chartBB.height - margin.top - margin.bottom;
+    margin = {top: 0, right: 13, bottom: xAxisTopHeight + xAxisBottomHeight, left: 0},
+    width = charwrapperBB.width - margin.left - margin.right,
+    height = charwrapperBB.height - margin.top - margin.bottom;
 
       
     // DESCRIPTION DIV
@@ -191,9 +191,15 @@
     
     var chart = d3.select(".chart")
         .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        .attr("height", height + margin.top + margin.bottom);
+        
+    chart.append("rect")
+      .attr("width", "100%")
+      .attr("height", "100%")
+      .attr("fill", "white");
+    
+    chart.append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
         
         
     // Y SCALE
@@ -259,6 +265,7 @@
       .append("text")
         .attr("text-anchor", "middle")
         .attr("transform", "translate(" + (width - yAxisWidth)/2 + ", -20)")
+        .style("text-anchor", "middle")
         .text("Medelvärde på en skala 0-100, där 5 poäng eller mer anses vara en relevant/märkbar skillnad");
     
         
@@ -276,6 +283,7 @@
       .append("text")
         .attr("text-anchor", "middle")
         .attr("transform", "translate(" + (width - yAxisWidth)/2 + ", 28)")
+        .style("text-anchor", "middle")
         .text("Medelvärde på en skala 0-100, där 5 poäng eller mer anses vara en relevant/märkbar skillnad");
 
 
@@ -323,7 +331,8 @@
             tooltipOut();
             setToNormalColor(i);
           })
-      .attr("transform", function(d, i) { return "translate(" + (yAxisWidth + x_scale(data.reference_map[d]) - referenceWidth/2) + "," + (y_scale(d) - referenceExtraHeight/2) + ")"; });
+      .attr("transform", function(d, i) { return "translate(" + (yAxisWidth + x_scale(data.reference_map[d]) - referenceWidth/2) + "," + (y_scale(d) - referenceExtraHeight/2) + ")"; })
+      .style("shape-rendering", "crispEdges");
 
     reference.append("rect")
       .attr("width", referenceWidth/2)
@@ -332,11 +341,18 @@
       
     // HEADER
     
+    var header_select_div = d3.select(".header-select-div");
     var header_select = d3.select(".header-select");
     var bar_area_width = (width - yAxisWidth);
-    var select_width = header_select.node().getBoundingClientRect().width;
-    header_select
+    var arrow_img_width = 33;
+    var select_width = header_select.node().getBoundingClientRect().width + arrow_img_width;
+    
+    header_select_div
+      .style("width", select_width + "px")
       .style("margin-left", yAxisWidth + bar_area_width/2 - select_width/2 + "px");
+      
+    header_select
+      .style("width", select_width+28 + "px")
       
       
     // TOOLTIP DIV
@@ -344,6 +360,26 @@
     /* only populate the description box, nothing visual */
     tooltipOver(0);
     tooltipOut();
+    
+    
+    // GENERAL STYLE
+    
+    d3.selectAll(".axis .domain")
+      .style("visibility", "hidden");
+    
+    d3.selectAll(".axis path, .axis line")
+      .style("fill", "none")
+      .style("stroke", "#bbb")
+      .style("shape-rendering", "geometricPrecision");
+      
+    d3.selectAll("text")
+      .style("fill", "#555")
+      .style("font", "12px sans-serif")
+      .style("cursor", "default")
+      
+    d3.selectAll(".y path, .y line")
+      .style("visibility", "hidden");
+
   }
   
   exports.updateSelecterOption = function(selected_perspective_option) {
@@ -361,4 +397,175 @@
     tooltipOut();
   }
   
+//   exports.generateImage = function() {
+//     var charwrapperBB = d3.select(".chart").node().getBBox();
+//     
+//     var canvas_chart = d3.select("body").append("canvas")
+//       .attr("id", "canvas-chart")
+//       .attr("width", charwrapperBB.width + "px")
+//       .attr("height", charwrapperBB.height + "px")
+//       .style("visibility", "hidden");   
+//     
+//     canvg(('canvas-chart'), d3.select(".chart").node().innerHTML);
+//     
+//     var header_text = data.perspective_options[perspective_option];
+//     var header_padding_top = 5;
+//     var font_size = 16;
+//     var text_padding_bottom = 10;
+//     var header_font = "bold " + font_size + "px sans-serif";
+//     var text_height = header_padding_top + font_size + text_padding_bottom;
+//         
+//     var complete_width = charwrapperBB.width;
+//     var complete_height = charwrapperBB.height + text_height;
+//     var canvas_complete = d3.select("body").append("canvas")
+//       .attr("id", "canvas-complete")
+//       .attr("width", complete_width + "px")
+//       .attr("height", complete_height + "px")
+//       .style("visibility", "visible");
+//       
+//     var ctx = canvas_complete.node().getContext("2d");
+//     
+//     ctx.fillStyle = "white";
+//     ctx.fillRect(0, 0, complete_width, complete_height);
+//     ctx.fillStyle = "black";
+//     
+//     ctx.font = header_font;
+// 
+//     var text_width = ctx.measureText(header_text).width;
+//     
+//     var bar_area_width = (width - yAxisWidth);
+//     var text_horizontal_shift = yAxisWidth + bar_area_width/2 - text_width/2;
+//     
+//     ctx.fillText(header_text, text_horizontal_shift, header_padding_top + font_size);
+//     ctx.drawImage(canvas_chart.node(), 0, text_height);
+//     
+//     canvas_chart.node().toBlob(function(blob) {
+//         saveAs(blob, header_text + ".png");
+//     });
+//     
+//     canvas_chart.remove();
+//     canvas_complete.remove();
+//   }
+
+// exports.generateImage = function() {
+//   var canvas_chart = d3.select("body").append("canvas")
+//     .attr("id", "canvas-chart")
+//     .attr("width", charwrapperBB.width + "px")
+//     .attr("height", charwrapperBB.height + "px")
+//     .style("position", "absolute")
+//     .style("top", "0px")
+//     .style("right", "0px")
+//     .style("visibility", "visible");   
+//       
+//   var ctx = canvas_chart.node().getContext("2d");
+//   
+//   var img = new Image(100, 200);
+//   img.onload = function() {
+//     ctx.drawImage(img, 0, 0);
+//     
+//     canvas_chart.node().toBlob(function(blob) {
+//       saveAs(blob, "test.png");
+//     });
+//   }
+//   
+// //   var xml = (new XMLSerializer).serializeToString( d3.select(".chart").node());
+// 
+//    var base64 = btoa('<svg xmlns="http://www.w3.org/2000/svg" height="100" width="100">' + d3.select(".chart").node().innerHTML + '</svg>');
+//   img.src = 'data:image/svg+xml;base64,' + base64;
+// 
+// //     img.src = 'data:image/svg+xml;base64,' + 
+// //            btoa('<svg xmlns="http://www.w3.org/2000/svg" height="100" width="100"><circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red"/></svg>');
+// 
+// //   img.src = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA1MTIgNTEyIj48cGF0aCBkPSJNMjI0IDM4Ny44MTRWNTEyTDMyIDMyMGwxOTItMTkydjEyNi45MTJDNDQ3LjM3NSAyNjAuMTUyIDQzNy43OTQgMTAzLjAxNiAzODAuOTMgMCA1MjEuMjg3IDE1MS43MDcgNDkxLjQ4IDM5NC43ODUgMjI0IDM4Ny44MTR6Ii8+PC9zdmc+";
+//   
+//   d3.select("body").node().appendChild(img);
+//   
+// }
+  exports.generateImage = function() {
+    var chartBB = d3.select(".chart").node().getBoundingClientRect();
+    
+    var chartWidth = chartBB.width;
+    var chartHeight = chartBB.height - xAxisTopHeight - xAxisBottomHeight + 10; // constant to fudge result
+    
+    var doctype = '<?xml version="1.0" standalone="no"?>'
+      + '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">';
+
+    var svg = d3.select('svg').node();
+    
+    var source = (new XMLSerializer()).serializeToString(svg);
+
+    var blob = new Blob([ doctype + source], { type: "image/svg+xml;charset=utf-8" });
+
+    var url = window.URL.createObjectURL(blob);
+
+    var img_selection = d3.select('body').append('img')
+      .attr("width", chartWidth)
+      .attr("height", chartHeight)
+      .style("visibility", "hidden");
+
+    img = img_selection.node();
+
+    img.onload = function(){
+      var canvas_chart_selection = d3.select('body').append('canvas')
+        .attr("width", chartWidth)
+        .attr("height", chartHeight)
+        .style("visibility", "hidden");
+      var canvas_chart = canvas_chart_selection.node();
+      
+      var chart_ctx = canvas_chart.getContext('2d');
+      chart_ctx.drawImage(img, 0, 0);
+      
+      var header_text = data.perspective_options[perspective_option];
+      var header_padding_top = 5;
+      var header_font_size = 16;
+      var header_padding_bottom = 10;
+      var header_font = "bold " + header_font_size + "px sans-serif";
+      var header_height = header_padding_top + header_font_size + header_padding_bottom;
+      
+      var img_margin = {top: 10, right: 20, bottom: 20, left: 10};
+          
+      var complete_width = img_margin.left + chartWidth + img_margin.right;
+      var complete_height = img_margin.top + header_height + chartHeight + img_margin.bottom;
+      var canvas_complete_selection = d3.select("body").append("canvas")
+        .attr("width", complete_width + "px")
+        .attr("height", complete_height + "px")
+        .style("visibility", "hidden");
+        
+      var canvas_complete = canvas_complete_selection.node();
+        
+      var ctx = canvas_complete_selection.node().getContext("2d");
+      
+      ctx.fillStyle = "white";
+      ctx.fillRect(0, 0, complete_width, complete_height);
+      ctx.fillStyle = "black";
+      
+      ctx.font = header_font;
+
+      var header_width = ctx.measureText(header_text).width;
+          
+      var bar_area_width = (width - yAxisWidth);
+      var header_horizontal_shift = yAxisWidth + bar_area_width/2 - header_width/2;
+      
+      ctx.fillText(header_text, header_horizontal_shift + img_margin.left, header_padding_top + header_font_size + img_margin.top);
+      
+      var source_text = "copsoq.se - en brygga mellan vetenskap och praktik i arbetsmiljöfrågor";
+      var source_font_height = 11;
+      ctx.font = source_font_height + "px sans-serif";
+      ctx.fillStyle = "#555";
+      var source_text_width = ctx.measureText(source_text).width;
+      ctx.fillText(source_text, 5, complete_height - 5);
+      
+      ctx.drawImage(canvas_chart, img_margin.left, img_margin.top + header_height);
+      
+      canvas_complete.toBlob(function(blob) {
+        saveAs(blob, header_text + ".png");
+      });
+      
+      img_selection.remove();
+      canvas_chart_selection.remove();
+      canvas_complete_selection.remove();
+    }
+
+    img.src = url;
+  }
 })(window);
