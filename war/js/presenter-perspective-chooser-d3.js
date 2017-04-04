093de8fe2dfb3b3e627e35738381265330d4fa84
Daniel Dunér <daniel.duner@gmail.com>
2017-04-04 22:28:57 +0200

    Add timeout hack to adjust gwt width after perspective picker is loaded for first time

--- war/js/presenter-perspective-chooser-d3.js
+++ war/js/presenter-perspective-chooser-d3.js
@@ -162,13 +162,22 @@
       .append('div')
         .classed('perspective-third-column', true);
 
+    initializeSelection();
+    initialized = true;
+    
+    // hack to force initial gwt sizing to work
+    // TODO replace when the resizing system is moved to pure js
+    for (var i=2; i<=12; i++) {
+      setTimeout(initializeSelection, Math.pow(2, i));
+    }
+  }
+  
+  function initializeSelection() {
     if (selected_perspective === -1) {
       setSelectedPerspective(0);
     } else {
-      updateCheckboxes(false);
+      setSelectedPerspective(selected_perspective);
     }
-    
-    initialized = true;
   }
   
   function setSelectedPerspective(index) {
