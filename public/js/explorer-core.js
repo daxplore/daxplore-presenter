a8afacaba6db2e630749717b65ba6a6f9e3e0ac7
Daniel Dunér <daniel.duner@gmail.com>
2019-01-13 19:43:17 +0100

    Move explorer.html initialization to separate js file

---
+++ public/js/explorer-core.js
@@ -0,0 +1,28 @@
+(function (exports) {
+  exports.initializeExplorer = function () {
+    // Use Axios to download all needed metadata files from the server
+    // Define functions for all metadata files to be downloaded
+    function getGroups () { return axios.get('data/groups.json') }
+    function getPerspectives () { return axios.get('data/perspectives.json') }
+    function getQuestions () { return axios.get('data/questions.json') }
+    function getSettings () { return axios.get('data/settings.json') }
+    function getUsertexts () { return axios.get('data/usertexts.json') }
+
+    // Make a batch Axios request to download all metadata asynchronously
+    axios.all([getGroups(), getPerspectives(), getQuestions(), getSettings(), getUsertexts()])
+    .then(axios.spread(function (groupsResponse, perspectivesResponse, questionsResponse, settingsResponse, usertextsResponse) {
+      // Get the downloaded metadata
+      let groups = groupsResponse.data
+      let perspectives = perspectivesResponse.data
+      let questions = questionsResponse.data
+      let settings = settingsResponse.data
+      let usertexts = usertextsResponse.data
+
+      // Initialize elements that depend on the metadata
+      // TODO don't use windows.?
+      window.generateQuestionPanel(questions, groups, usertexts, settings)
+      window.generatePerspectivePanel(questions, perspectives, usertexts, settings)
+      window.generateChartPanel(questions, groups, null, null) // TODO fix constructor
+    }))
+  }
+})(window)
